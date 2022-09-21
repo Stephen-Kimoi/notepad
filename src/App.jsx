acce45f59ae5140565dd5f875e3c1904cf4c9e7f
Steve Kimoi <moenisteve@gmail.com>
2022-09-21 11:48:42 +0300

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import { Sidebar } from './components/Sidebar/Sidebar'
+import Editor from './components/Editor/Editor' 
+import { data } from './assets/data/data'
+import Split from 'react-split'
+import { nanoid } from 'nanoid'
+import './App.css'
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+function App() {
+  const [notes, setNotes] = useState([]) 
+  const [currentNoteId, setCurrentNoteId] = useState( 
+    (notes[0] && notes[0].id) || ""
+  ) 
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(), 
+      body: "# Type your markdown note's title here"
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes]) 
+    setCurrentNoteId(newNote.id) 
+    console.log(newNote.id)
+  }
+
+  function updateNote(text) {
+    setNotes(oldNotes => oldNotes.map(oldNote => {
+      return oldNote.id === currentNoteId 
+          ? {...oldNote, body: text} 
+          : oldNote 
+    }))
+  }
+
+  // Research more on this function cause I don't understand
+  function findCurrentNote() {
+    return notes.find(note => {
+      return note.id === currentNoteId
+    }) || notes[0]
+  }
+
+  return (
+     <main>
+       {
+         notes.length > 0 
+         ? 
+         <Split
+            sizes={[30,70]}
+            direction="horizontal" 
+            className='split'
+         >
+         <Sidebar 
+           notes={notes} 
+           currentNote={findCurrentNote()} 
+           setCurrentNoteId={setCurrentNoteId}
+           newNote={createNewNote}
+         />
+         {
+           currentNoteId && notes.length > 0 && 
+           <Editor 
+             currentNote={findCurrentNote()} 
+             updateNote={updateNote}
+           /> 
+         }
+         </Split>
+         : 
+         <div className='no-notes'>
+             <h1>You have no notes</h1>
+             <button
+                className='first-note'
+                onClick={createNewNote}
+             >
+                 Create new note
+             </button>
+         </div>
+       }
+     </main>
+  )
+}
+
+export default App
